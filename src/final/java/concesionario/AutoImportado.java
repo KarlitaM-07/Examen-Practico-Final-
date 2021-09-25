77645e81e468d3c025621c555f61c773e11a146b
Karla Chipantasi <belenchipantai@gmail.com>
2021-09-24 19:49:27 -0500

    Primera Parte

---
+++ src/final/java/concesionario/AutoImportado.java
@@ -0,0 +1,57 @@
+package concesionario;
+
+public class AutoImportado {
+	
+	private String marca;
+	private String modelo;
+	private String añoFabricacion;
+	private String placa;
+	private String kilometraje;
+	private String codigoImportacion;
+	private String valorImportacion;
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getAñoFabricacion() {
+		return añoFabricacion;
+	}
+	public void setAñoFabricacion(String añoFabricacion) {
+		this.añoFabricacion = añoFabricacion;
+	}
+	public String getKilometraje() {
+		return kilometraje;
+	}
+	public void setKilometraje(String kilometraje) {
+		this.kilometraje = kilometraje;
+	}
+	public String getCodigoImportacion() {
+		return codigoImportacion;
+	}
+	public void setCodigoImportacion(String codigoImportacion) {
+		this.codigoImportacion = codigoImportacion;
+	}
+	public String getValorImportacion() {
+		return valorImportacion;
+	}
+	public void setValorImportacion(String valorImportacion) {
+		this.valorImportacion = valorImportacion;
+	}
+
+}
---
+++ src/final/java/concesionario/AutoNacional.java
@@ -0,0 +1,43 @@
+package concesionario;
+
+public class AutoNacional {
+	
+	private String marca;
+	private String modelo;
+	private String añoFabricacion;
+	private String placa;
+	private String kilometraje;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getAñoFabricacion() {
+		return añoFabricacion;
+	}
+	public void setAñoFabricacion(String añoFabricacion) {
+		this.añoFabricacion = añoFabricacion;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getKilometraje() {
+		return kilometraje;
+	}
+	public void setKilometraje(String kilometraje) {
+		this.kilometraje = kilometraje;
+	}
+	
+
+}
